330eee474e2a2970d828fde72edf0f5fde423558
jiayi <jiaasayang@jiayi.local>
2013-04-10 17:02:18 +0800

    Initial Commit

---
+++ Sound/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sound
+//
+//  Created by jiayi on 13-4-10.
+//  Copyright (c) 2013年 jiayi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
